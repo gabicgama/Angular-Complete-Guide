73efaa1d1edb5cd75785afa94521e5511af5cd89
gabriela <gabycgama@hotmail.com>
2022-09-10 21:03:02 -0300

    selectors alternatives

--- myApp/src/app/servers/servers.component.ts
+++ myApp/src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  //selecttor: 'app-servers', //default
+  //selector: '[app-servers]', //atributo
+  selector: '.app-servers', //classe
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
